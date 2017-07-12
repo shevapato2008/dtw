40b0c49b2bed7f7b4400fd9aa2e1d4526f836e28
shevapato2008 <shevapato2008@gmail.com>
2017-07-12 15:06:00 -0400

    Modified.

--- knn_dtw/main.py
+++ knn_dtw/main.py
@@ -1,3 +1,41 @@
-import sys
-import collections
-import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+
+from classes import KnnDtw
+
+plt.style.use('bmh')
+%matplotlib inline
+
+try:
+    from IPython.display import clear_output
+    have_ipython = True
+except ImportError:
+    have_ipython = False
+
+'''
+Measuring the DTW distance
+'''
+
+
+time = np.linspace(0,20,1000)
+amplitude_a = 5*np.sin(time)
+amplitude_b = 3*np.sin(time + 1)
+
+m = KnnDtw()
+distance = m._dtw_distance(amplitude_a, amplitude_b)
+
+fig = plt.figure(figsize=(12,4))
+_ = plt.plot(time, amplitude_a, label='A')
+_ = plt.plot(time, amplitude_b, label='B')
+_ = plt.title('DTW distance between A and B is %.2f' % distance)
+_ = plt.ylabel('Amplitude')
+_ = plt.xlabel('Time')
+_ = plt.legend()
+
+
+
+
+
+
+
+
